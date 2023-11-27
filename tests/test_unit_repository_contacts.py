195280055a425a4c1d99cd7c0a3b574756752e28
lexxai <lexxai@users.noreply.github.com>
2023-11-27 08:53:43 +0200

    test_update_favorite_contact_found

--- tests/test_unit_repository_contacts.py
+++ tests/test_unit_repository_contacts.py
@@ -91,118 +91,29 @@ class TestContacts(unittest.IsolatedAsyncioTestCase):
         body = ContactModel(first_name="test1-1", last_name="test2-1", email="aa@uu.uu", phone="+380 (44) 1234567")
         self.session.query().filter_by().first.return_value = contact
         self.session.commit.return_value = None
-        result = await update(contact_id=1, body=body, user_id=self.user.id, db=self.session) # type: ignore
+        result = await update(contact_id=1, body=body, user_id=self.user.id, db=self.session)  # type: ignore
         self.assertEqual(result, contact)
-
 
     async def test_update_contact_not_found(self):
         body = ContactModel(first_name="test1-1", last_name="test2-1", email="aa@uu.uu", phone="+380 (44) 1234567")
         self.session.query().filter_by().first.return_value = None
         self.session.commit.return_value = None
-        result = await update(contact_id=1, body=body, user_id=self.user.id, db=self.session) # type: ignore
+        result = await update(contact_id=1, body=body, user_id=self.user.id, db=self.session)  # type: ignore
         self.assertIsNone(result)
 
     async def test_update_favorite_contact_found(self):
         body = ContactFavoriteModel(favorite=True)
         contact = Contact()
         self.session.query().filter_by().first.return_value = contact
-        result = await favorite_update(contact_id=1, body=body, user_id=self.user.id, db=self.session) # type: ignore
+        result = await favorite_update(contact_id=1, body=body, user_id=self.user.id, db=self.session)  # type: ignore
         self.assertEqual(result, contact)
 
-
-
-#     async def test_update_status_note_not_found(self):
-#         body = NoteStatusUpdate(done=True)
-#         self.session.query().filter().first.return_value = None
-#         self.session.commit.return_value = None
-#         result = await update_status_note(note_id=1, body=body, user=self.user, db=self.session)
-#         self.assertIsNone(result)
-
-
-
-
-
-
-
-
-
-
-
-# class TestNotes(unittest.IsolatedAsyncioTestCase):
-
-#     def setUp(self):
-#         self.session = MagicMock(spec=Session)
-#         self.user = User(id=1)
-
-#     async def test_get_notes(self):
-#         notes = [Note(), Note(), Note()]
-#         self.session.query().filter().offset().limit().all.return_value = notes
-#         result = await get_notes(skip=0, limit=10, user=self.user, db=self.session)
-#         self.assertEqual(result, notes)
-
-#     async def test_get_note_found(self):
-#         note = Note()
-#         self.session.query().filter().first.return_value = note
-#         result = await get_note(note_id=1, user=self.user, db=self.session)
-#         self.assertEqual(result, note)
-
-#     async def test_get_note_not_found(self):
-#         self.session.query().filter().first.return_value = None
-#         result = await get_note(note_id=1, user=self.user, db=self.session)
-#         self.assertIsNone(result)
-
-#     async def test_create_note(self):
-#         body = NoteModel(title="test", description="test note", tags=[1, 2])
-#         tags = [Tag(id=1, user_id=1), Tag(id=2, user_id=1)]
-#         self.session.query().filter().all.return_value = tags
-#         result = await create_note(body=body, user=self.user, db=self.session)
-#         self.assertEqual(result.title, body.title)
-#         self.assertEqual(result.description, body.description)
-#         self.assertEqual(result.tags, tags)
-#         self.assertTrue(hasattr(result, "id"))
-
-#     async def test_remove_note_found(self):
-#         note = Note()
-#         self.session.query().filter().first.return_value = note
-#         result = await remove_note(note_id=1, user=self.user, db=self.session)
-#         self.assertEqual(result, note)
-
-#     async def test_remove_note_not_found(self):
-#         self.session.query().filter().first.return_value = None
-#         result = await remove_note(note_id=1, user=self.user, db=self.session)
-#         self.assertIsNone(result)
-
-#     async def test_update_note_found(self):
-#         body = NoteUpdate(title="test", description="test note", tags=[1, 2], done=True)
-#         tags = [Tag(id=1, user_id=1), Tag(id=2, user_id=1)]
-#         note = Note(tags=tags)
-#         self.session.query().filter().first.return_value = note
-#         self.session.query().filter().all.return_value = tags
-#         self.session.commit.return_value = None
-#         result = await update_note(note_id=1, body=body, user=self.user, db=self.session)
-#         self.assertEqual(result, note)
-
-#     async def test_update_note_not_found(self):
-#         body = NoteUpdate(title="test", description="test note", tags=[1, 2], done=True)
-#         self.session.query().filter().first.return_value = None
-#         self.session.commit.return_value = None
-#         result = await update_note(note_id=1, body=body, user=self.user, db=self.session)
-#         self.assertIsNone(result)
-
-#     async def test_update_status_note_found(self):
-#         body = NoteStatusUpdate(done=True)
-#         note = Note()
-#         self.session.query().filter().first.return_value = note
-#         self.session.commit.return_value = None
-#         result = await update_status_note(note_id=1, body=body, user=self.user, db=self.session)
-#         self.assertEqual(result, note)
-
-#     async def test_update_status_note_not_found(self):
-#         body = NoteStatusUpdate(done=True)
-#         self.session.query().filter().first.return_value = None
-#         self.session.commit.return_value = None
-#         result = await update_status_note(note_id=1, body=body, user=self.user, db=self.session)
-#         self.assertIsNone(result)
+    async def test_update_favorite_contact_not_found(self):
+        body = ContactFavoriteModel(favorite=True)
+        contact = Contact()
+        self.session.query().filter_by().first.return_value = None
+        result = await favorite_update(contact_id=1, body=body, user_id=self.user.id, db=self.session)  # type: ignore
+        self.assertIsNone(result)
 
 
 if __name__ == "__main__":
