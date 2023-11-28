4626cad0b37c8f5494739373f96049e2a68034a2
lexxai <lexxai@users.noreply.github.com>
2023-11-28 10:24:42 +0200

    SUPPRESS REDIS CONNECTION POOL

--- tests/test_pytest_route_contacts.py
+++ tests/test_pytest_route_contacts.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import os
 from pathlib import Path
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 import pytest
 
@@ -11,6 +11,8 @@ os.environ["PYTHONPATH"] += os.pathsep + hw_path
 
 from src.database.models import Contact, User
 from src.shemas.contact import ContactModel
+#from src.services.auth.auth import auth_service
+from src.database import db
 
 
 def create_user(client, session, user, monkeypatch):
@@ -20,7 +22,7 @@ def create_user(client, session, user, monkeypatch):
         "/api/auth/signup",
         json=user,
     )
-    current_user: User = session.query(User).filter(User.email == user.get('email')).first()
+    current_user: User = session.query(User).filter(User.email == user.get("email")).first()
     current_user.confirmed = True
     session.commit()
     assert response.status_code == 201, response.text
@@ -32,12 +34,11 @@ def create_user(client, session, user, monkeypatch):
 def get_access_token_user(client, user):
     response = client.post(
         "/api/auth/login",
-        data={"username": user.get('email'), "password": user.get('password')},
+        data={"username": user.get("email"), "password": user.get("password")},
     )
     data = response.json()
     access_token = data["access_token"]
     return f"Bearer {access_token}"
-
 
 
 @pytest.fixture()
@@ -46,12 +47,27 @@ def token(client, user, session, monkeypatch):
     return get_access_token_user(client, user)
 
 
-def test_create_contact(client, contact, token ):
-    headers = {
-        "Authorization": token
-    }
-    response = client.post("/api/contacts", headers=headers,  json=contact )
-    assert response.status_code == 201, response.text
-    data = response.json()
-    assert data["first_name"] == contact.get("first_name")
-    assert "id" in data
+def s_test_create_contact_0(client, contact, token):
+    with patch.object(db, 'redis_pool') as r_mock:
+        r_mock.get.return_value = None
+        headers = {"Authorization": token}
+        response = client.post("/api/contacts", headers=headers, json=contact)
+        assert response.status_code == 201, response.text
+        data = response.json()
+        assert data["first_name"] == contact.get("first_name")
+        assert "id" in data
+
+
+def test_create_contact(client, contact, token):
+    # SUPPRESS REDIS CONNECTION POOL  !!! is  redis_pool
+    with patch.object(db, 'redis_pool') as r_mock:
+        r_mock.get.return_value = None
+        response = client.post(
+            "/api/contacts",
+            json=contact,
+            headers={"Authorization": token}
+        )
+        assert response.status_code == 201, response.text
+        data = response.json()
+        assert data["first_name"] == contact.get("first_name")
+        assert "id" in data
