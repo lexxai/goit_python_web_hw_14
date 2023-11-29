366bcc95d3a975f3025281192710f354a8939362
lexxai <lexxai@users.noreply.github.com>
2023-11-29 20:18:29 +0200

    pytest-cover

--- tests/test_pytest_route_contacts.py
+++ tests/test_pytest_route_contacts.py
@@ -9,39 +9,35 @@ hw_path: str = str(Path(__file__).resolve().parent.parent.joinpath("hw14"))
 os.environ["PATH"] += os.pathsep + hw_path
 os.environ["PYTHONPATH"] += os.pathsep + hw_path
 
-from src.database.models import Contact, User
-from src.shemas.contact import ContactModel
-
-# from src.services.auth.auth import auth_service
-from src.database import db
+from src.database.models import User
 
 
-class MockRedis:
-    async def get_connection(self, *args):
-        print("MockRedis get_connection")
-        return False
+# class MockRedis:
+#     async def get_connection(self, *args):
+#         print("MockRedis get_connection")
+#         return MockRedis
 
-    async def get(self, *args):
-        print("MockRedis get")
-        return True
+#     async def get(self, *args):
+#         print("MockRedis get")
+#         return None
 
-    async def set(self, *args):
-        print("MockRedis set")
-        return True
+#     async def set(self, *args):
+#         print("MockRedis set")
+#         return None
 
-    async def expire(self, *args):
-        print("MockRedis expire")
-        return True
+#     async def expire(self, *args):
+#         print("MockRedis expire")
+#         return None
 
-    async def release(self, *args):
-        print("MockRedis release")
-        return True
+#     async def release(self, *args):
+#         print("MockRedis release")
+#         return None
 
 
 def create_user(client, session, user, monkeypatch):
     mock_send_email = MagicMock()
     monkeypatch.setattr("src.services.emails.send_email", mock_send_email)
-    
+
     response = client.post(
         "/api/auth/signup",
         json=user,
@@ -49,10 +45,6 @@ def create_user(client, session, user, monkeypatch):
     current_user: User = session.query(User).filter(User.email == user.get("email")).first()
     current_user.confirmed = True
     session.commit()
-    # assert response.status_code == 201, response.text
-    # data = response.json()
-    # assert data["user"]["email"] == user.get("email")
-    # assert "id" in data["user"]
 
 
 def get_access_token_user(client, user):
@@ -65,33 +57,18 @@ def get_access_token_user(client, user):
     return f"Bearer {access_token}"
 
 
-
-
 @pytest.fixture()
 def token(client, user, session, monkeypatch, mock_ratelimiter):
+    """ get auth token foa all auth requests
+        mock_ratelimiter not used, but required in argumants for execute fixture before
+    """
     # print(f"token {db.redis_pool=}")
     create_user(client, session, user, monkeypatch)
     return get_access_token_user(client, user)
 
 
-def s_test_create_contact_0(client, contact, token):
-    with patch("src.database.db.redis_pool", False):
-        headers = {"Authorization": token}
-        response = client.post("/api/contacts", headers=headers, json=contact)
-        assert response.status_code == 201, response.text
-        data = response.json()
-        assert data["first_name"] == contact.get("first_name")
-        assert "id" in data
-
-
-# @patch("src.database.db.get_redis", gr)
-
-
 # @patch("src.database.db.redis_pool", False)
 def test_create_contact(client, contact, token):
-    # db.redis_pool = MagicMock(spec=MockRedis)
-    # SUPPRESS REDIS CONNECTION POOL  !!! is  redis_pool
-    # with patch("src.database.db.redis_pool", False):
     response = client.post("/api/contacts", json=contact, headers={"Authorization": token})
     assert response.status_code == 201, response.text
     data = response.json()
@@ -153,9 +130,6 @@ def test_delete_contact(client, token):
     # with patch("src.database.db.redis_pool", False):
     response = client.delete("/api/contacts/1", headers={"Authorization": token})
     assert response.status_code == 204, response.text
-    # data = response.json()
-    # assert data["email"] == "new@email.com"
-    # assert "id" in data
 
 
 # @patch("src.database.db.redis_pool", False)
